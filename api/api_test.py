8b421ff3234fce868bfd3e06566cc2d7547d884c
Dhruv Mishra <ubiquitiousdm@gmail.com>
2019-11-14 15:53:57 +0530

    switching the project id to default

--- api/api_test.py
+++ api/api_test.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-BASE_URL = "https://semantic-similarity-dot-content-audit-tool.appspot.com/"
-
+# BASE_URL = "https://semantic-similarity-dot-content-audit-tool.appspot.com/"
+BASE_URL = "https://content-audit-tool.appspot.com/"
 # BASE_URL = "http://127.0.0.1:5000/"
 
 email = "aparna@gmail.com"
